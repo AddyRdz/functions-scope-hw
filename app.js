3ef03c798d4e931ca6b4f399f4e4c2cc600ad631
Addy Rodriguez <rdz.33.a@gmail.com>
2021-12-16 18:35:10 -0600

    Commit 1- Verbal Questions.

---
+++ app.js
@@ -0,0 +1,8 @@
+1. Parameters is the value received by the function while an argument is the value that is passed to the function. 
+So to me the parameter is sort of like having an acre of land and the argument is what you send into the acre of land 
+but you don't go before or over your acrage. 
+
+2.Console.log is the function that sends the message and return is what we put to define the output. Ex, listing the 
+alphabet in ordere but asking in return the vowels only.
+
+3. You run the risk of running a loop and not receiving a return.
